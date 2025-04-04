f36afcbcd9f39101c99abfe74c7cee486f8a4cc5
吴伊玲 <woo12310@163.com>
2025-04-04 15:31:36 +0800

    the login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,61 +2,68 @@
     <div class="container">
         <div class="content">
             <text class="title">Login</text>
-            <a-form :model="formState" name="normal_login" class="login-form" @finish="onFinish"
-                @finishFailed="onFinishFailed">
-                <a-form-item label="账号" name="accountid"
-                    :rules="[{ required: true, message: '请输入账号' }]">
-                    <a-input v-model:value="formState.accountid" allow-clear placeholder="手机号/邮箱/用户名">
-                    </a-input>
-                </a-form-item>
-
-                <a-form-item label="密码" name="password"
-                    :rules="[{ required: true, message: '请输入密码！' }]">
-                    <a-input-password v-model:value="formState.password" allow-clear placeholder="请输入密码">
-                    </a-input-password>
-                </a-form-item>
-
-                <a-form-item>
-                    <a-form-item name="remember" no-style>
-                        <a-checkbox v-model:checked="formState.remember">记住我</a-checkbox>
+            <a-spin :spinning="loading" tip="登录中...">
+                <a-form :model="formState" name="normal_login" class="login-form" @finish="onFinish"
+                    @finishFailed="onFinishFailed">
+                    <a-form-item label="账号" name="accountid" :rules="[{ required: true, message: '请输入账号' }]">
+                        <a-input v-model:value="formState.accountid" allow-clear placeholder="手机号/邮箱/用户名">
+                        </a-input>
                     </a-form-item>
-                </a-form-item>
 
-                <a-form-item>
-                    <a-button type="primary" html-type="submit" class="login-form-button">
-                        登录
-                    </a-button>
-                </a-form-item>
-            </a-form>
+                    <a-form-item label="密码" name="password" :rules="[{ required: true, message: '请输入密码！' }]">
+                        <a-input-password v-model:value="formState.password" allow-clear placeholder="请输入密码">
+                        </a-input-password>
+                    </a-form-item>
+
+                    <a-form-item>
+                        <a-form-item name="remember" no-style>
+                            <a-checkbox v-model:checked="formState.remember">记住我</a-checkbox>
+                        </a-form-item>
+                    </a-form-item>
+
+                    <a-form-item>
+                        <a-button type="primary" html-type="submit" class="login-form-button">
+                            登录
+                        </a-button>
+                    </a-form-item>
+                </a-form>
+            </a-spin>
         </div>
     </div>
 </template>
 
 <script setup>
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
 import userApi from '@/api/user';
-// import { useRouter } from 'vue-router';
+import { useRouter } from 'vue-router';
 
-// const router = useRouter();
+const router = useRouter();
 
+const loading=ref(false)
 const formState = reactive({
     accountid: '',
     password: '',
     remember: true,
 });
-const onFinish = values => {
+const onFinish = async (values) => {
+    loading.value=true
     try {
-        const res = userApi.login({
-            accountid: values.accountid,
+        const res = await userApi.login({
+            username: values.accountid,
             password: values.password,
         });
         console.log('res:', res);
+        if (res.status === 200) {
+            router.push('/home')
+        } else {
+            throw new Error("登录失败");
+        }
     } catch (error) {
+        alert('账号或密码错误')
         console.log('error:', error);
+    }finally{
+        loading.value=false
     }
-    // router.push('/home');
-    console.log('Success:', values);
-    console.log('formState:', formState.value);
 };
 const onFinishFailed = errorInfo => {
     console.log('Failed:', errorInfo);
